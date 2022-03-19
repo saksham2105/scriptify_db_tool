e2f345a346fd16b8f155cc82bdad9989eba377e2
saksham2105 <solankisaksham1998@gmail.com>
2022-03-19 22:20:40 +0530

    Added Feature to Pull and Shrink size of Table

--- examples/drawing.js
+++ examples/drawing.js
@@ -17,6 +17,10 @@ var currentTableHighlightedIndex = 0;
 var mousemove=false;
 var username = "";
 var color = "black";
+var increaseParameter = "";
+var lastModifiedTableDimension = null;
+var direction = "";
+var factor = 0;
 function setColor(c) {
    color = c;
    draw(ctx);
@@ -46,6 +50,14 @@ this.name="";
 this.Fields=[];
 this.note="";
 this.engine="";
+this.leftMidX = 0;
+this.leftMidY = 0;
+this.rightMidX = 0;
+this.rightMidY = 0;
+this.topMidY = 0;
+this.topMidX = 0;
+this.bottomMidX = 0;
+this.bottomMidY = 0;
 }
 }
 
@@ -83,10 +95,47 @@ function setup(canvasid)
     }  
   function elementDrag(e) {
     let rectt=canvas.getBoundingClientRect();
-    let cx=event.clientX-rectt.left;
-    let cy=event.clientY-rectt.top;
+    let cx=e.clientX-rectt.left;
+    let cy=e.clientY-rectt.top;
     e = e || window.event;
     e.preventDefault();
+    /**
+     * Code To Increase of Decrease Height and Width of Table
+    */
+    for(var t of Tables)
+    {
+    /**Left Mid X and Y */
+    if(cx>=t.leftMidX-4 && cx<=t.leftMidX-4+9 && cy>=t.leftMidY-4 && cy<=t.leftMidY-4+9)
+    {
+     direction = "left"; 
+     lastModifiedTableDimension = t;
+     increaseParameter = "width";
+    }
+    /**Right Mid X and Y */
+    if(cx>=t.rightMidX-4 && cx<=t.rightMidX-4+9 && cy>=t.rightMidY-4 && cy<=t.rightMidY-4+9)
+    {
+      direction = "right"; 
+      lastModifiedTableDimension = t;
+      increaseParameter = "width";
+    }
+    /**Top Mid X and Y */
+    if(cx>=t.topMidX-4 && cx<=t.topMidX-4+9 && cy>=t.topMidY-4 && cy<=t.topMidY-4+9)
+    {
+      direction = "top"; 
+      lastModifiedTableDimension = t;
+      increaseParameter = "height";
+    }
+    /**Bottom Mid X and Y */
+    if(cx>=t.bottomMidX-4 && cx<=t.bottomMidX-4+9 && cy>=t.bottomMidY-4 && cy<=t.bottomMidY-4+9)
+    {
+      direction = "bottom"; 
+      lastModifiedTableDimension = t;
+      increaseParameter = "height";
+    }
+   }
+   if (lastModifiedTableDimension != null) {
+     return;
+   }
     for(var t of Tables)
     {
     if(cx>=t.x && cx<=t.x+t.width && cx>=t.y && cy<=t.y+t.height)
@@ -98,10 +147,19 @@ function setup(canvasid)
     pos4 = e.clientY;
     t.x=t.x-pos1;
     t.y=t.y-pos2;
+    t.leftMidX = t.x;
+    t.leftMidY = t.y+t.height/2;
+    t.rightMidX = t.x+t.width;
+    t.rightMidY = t.y+t.height/2;
+    t.topMidX = t.x+t.width/2;
+    t.topMidY = t.y;
+    t.bottomMidX = t.x+t.width/2;
+    t.bottomMidY = t.y+t.height;
     }
   }
   draw(ctx);
  }
+  
   
   function closeDragElement(e) {
     let rectt=canvas.getBoundingClientRect();
@@ -110,6 +168,240 @@ function setup(canvasid)
     e = e || window.event;
     //mycanvas.removeAttribute("style","cursor:move");
     e.preventDefault();
+    if (lastModifiedTableDimension != null && lastModifiedTableDimension != undefined) {
+      if (increaseParameter == "width") {
+        if (cx < lastModifiedTableDimension.x && direction == "left") {
+          for (var t of Tables) {
+            /**Pulling from left */
+            if (t.name == lastModifiedTableDimension.name) {
+              console.log("Pulling from "+direction);
+              t.width = t.width+Math.abs(t.x-cx);
+              t.x = cx;
+              t.leftMidX = t.x;
+              t.leftMidY = t.y+t.height/2;
+              t.rightMidX = t.x+t.width;
+              t.rightMidY = t.y+t.height/2;
+              t.topMidX = t.x+t.width/2;
+              t.topMidY = t.y;
+              t.bottomMidX = t.x+t.width/2;
+              t.bottomMidY = t.y+t.height;
+              increaseParameter = "";
+              direction = "";
+              factor = 0;
+              lastModifiedTableDimension = null;
+              projectLocalStorage.removeItem("tables");
+              projectLocalStorage.setItem("tables",JSON.stringify(Tables));
+              canvas.onmouseup = null;
+              canvas.onmousemove = null;
+              canvas.removeAttribute("style","cursor:move");
+              draw(ctx);
+              return;
+            }
+          } 
+        }
+        if (cx > lastModifiedTableDimension.x +lastModifiedTableDimension.width && direction == "right" ) {
+          for (var t of Tables) {
+            /**Pulling from right */
+            if (t.name == lastModifiedTableDimension.name) {
+              console.log("Pulling from "+direction);
+              t.width = t.width+Math.abs(t.x-cx);
+              t.leftMidX = t.x;
+              t.leftMidY = t.y+t.height/2;
+              t.rightMidX = t.x+t.width;
+              t.rightMidY = t.y+t.height/2;
+              t.topMidX = t.x+t.width/2;
+              t.topMidY = t.y;
+              t.bottomMidX = t.x+t.width/2;
+              t.bottomMidY = t.y+t.height;
+              increaseParameter = "";
+              direction = "";
+              factor = 0;
+              lastModifiedTableDimension = null;
+              projectLocalStorage.removeItem("tables");
+              projectLocalStorage.setItem("tables",JSON.stringify(Tables));
+              canvas.onmouseup = null;
+              canvas.onmousemove = null;
+              canvas.removeAttribute("style","cursor:move");
+              draw(ctx);
+              return;
+            }
+          } 
+        } 
+        if (cx > lastModifiedTableDimension.x && cx<lastModifiedTableDimension.x+lastModifiedTableDimension.width && direction == "left") {
+          for (var t of Tables) {
+            /**Shrinking from left */
+            if (t.name == lastModifiedTableDimension.name) {
+              console.log("Shrinking from "+direction);
+              t.width = t.width-Math.abs(t.x-cx);
+              t.x = cx;
+              t.leftMidX = t.x;
+              t.leftMidY = t.y+t.height/2;
+              t.rightMidX = t.x+t.width;
+              t.rightMidY = t.y+t.height/2;
+              t.topMidX = t.x+t.width/2;
+              t.topMidY = t.y;
+              t.bottomMidX = t.x+t.width/2;
+              t.bottomMidY = t.y+t.height;
+              increaseParameter = "";
+              direction = "";
+              factor = 0;
+              lastModifiedTableDimension = null;
+              projectLocalStorage.removeItem("tables");
+              projectLocalStorage.setItem("tables",JSON.stringify(Tables));
+              canvas.onmouseup = null;
+              canvas.onmousemove = null;
+              canvas.removeAttribute("style","cursor:move");
+              draw(ctx);
+              return;
+            }
+          } 
+        }
+        if (cx > lastModifiedTableDimension.x && cx<lastModifiedTableDimension.x +lastModifiedTableDimension.width && direction == "right" ) {
+          for (var t of Tables) {
+            /**Shrinking from right */
+            if (t.name == lastModifiedTableDimension.name) {
+              console.log("Shrinking from "+direction);
+              t.width = t.width-Math.abs(t.x+t.width-cx);
+              t.leftMidX = t.x;
+              t.leftMidY = t.y+t.height/2;
+              t.rightMidX = t.x+t.width;
+              t.rightMidY = t.y+t.height/2;
+              t.topMidX = t.x+t.width/2;
+              t.topMidY = t.y;
+              t.bottomMidX = t.x+t.width/2;
+              t.bottomMidY = t.y+t.height;
+              increaseParameter = "";
+              direction = "";
+              factor = 0;
+              lastModifiedTableDimension = null;
+              projectLocalStorage.removeItem("tables");
+              projectLocalStorage.setItem("tables",JSON.stringify(Tables));
+              canvas.onmouseup = null;
+              canvas.onmousemove = null;
+              canvas.removeAttribute("style","cursor:move");
+              draw(ctx);
+              return;
+            }
+          } 
+        }
+      }
+      if (increaseParameter == "height") {
+        if (cy < lastModifiedTableDimension.y && direction == "top") {
+          for (var t of Tables) {
+            /**Pulling from top */
+            if (t.name == lastModifiedTableDimension.name) {
+              console.log("Pulling from "+direction);
+              t.height = t.height+Math.abs(t.y-cy);
+              t.y = cy;
+              t.leftMidX = t.x;
+              t.leftMidY = t.y+t.height/2;
+              t.rightMidX = t.x+t.width;
+              t.rightMidY = t.y+t.height/2;
+              t.topMidX = t.x+t.width/2;
+              t.topMidY = t.y;
+              t.bottomMidX = t.x+t.width/2;
+              t.bottomMidY = t.y+t.height;
+              increaseParameter = "";
+              direction = "";
+              factor = 0;
+              lastModifiedTableDimension = null;
+              projectLocalStorage.removeItem("tables");
+              projectLocalStorage.setItem("tables",JSON.stringify(Tables));
+              canvas.onmouseup = null;
+              canvas.onmousemove = null;
+              canvas.removeAttribute("style","cursor:move");
+              draw(ctx);
+              return;
+            }
+          } 
+        }
+        if (cy > lastModifiedTableDimension.y +lastModifiedTableDimension.height && direction == "bottom" ) {
+          for (var t of Tables) {
+            /**Pulling from bottom */
+            if (t.name == lastModifiedTableDimension.name) {
+              console.log("Pulling from "+direction);
+              t.height = t.height+Math.abs(t.y-cy);
+              t.leftMidX = t.x;
+              t.leftMidY = t.y+t.height/2;
+              t.rightMidX = t.x+t.width;
+              t.rightMidY = t.y+t.height/2;
+              t.topMidX = t.x+t.width/2;
+              t.topMidY = t.y;
+              t.bottomMidX = t.x+t.width/2;
+              t.bottomMidY = t.y+t.height;
+              increaseParameter = "";
+              direction = "";
+              factor = 0;
+              lastModifiedTableDimension = null;
+              projectLocalStorage.removeItem("tables");
+              projectLocalStorage.setItem("tables",JSON.stringify(Tables));
+              canvas.onmouseup = null;
+              canvas.onmousemove = null;
+              canvas.removeAttribute("style","cursor:move");
+              draw(ctx);
+              return;
+            }
+          } 
+        } 
+        if (cy > lastModifiedTableDimension.y && cy < lastModifiedTableDimension.y+lastModifiedTableDimension.height && direction == "top") {
+          for (var t of Tables) {
+            /**Shrinking from top */
+            if (t.name == lastModifiedTableDimension.name) {
+              console.log("Shrinking from "+direction);
+              t.height = t.height-Math.abs(t.y-cy);
+              t.y = cy;
+              t.leftMidX = t.x;
+              t.leftMidY = t.y+t.height/2;
+              t.rightMidX = t.x+t.width;
+              t.rightMidY = t.y+t.height/2;
+              t.topMidX = t.x+t.width/2;
+              t.topMidY = t.y;
+              t.bottomMidX = t.x+t.width/2;
+              t.bottomMidY = t.y+t.height;
+              increaseParameter = "";
+              direction = "";
+              factor = 0;
+              lastModifiedTableDimension = null;
+              projectLocalStorage.removeItem("tables");
+              projectLocalStorage.setItem("tables",JSON.stringify(Tables));
+              canvas.onmouseup = null;
+              canvas.onmousemove = null;
+              canvas.removeAttribute("style","cursor:move");
+              draw(ctx);
+              return;
+            }
+          } 
+        }
+        if (cy < lastModifiedTableDimension.y+lastModifiedTableDimension.height && direction == "bottom") {
+          for (var t of Tables) {
+            /**Shrinking from bottom */
+            if (t.name == lastModifiedTableDimension.name) {
+              console.log("Shrinking from "+direction);
+              t.height = t.height-Math.abs(t.y+t.height-cy);
+              t.leftMidX = t.x;
+              t.leftMidY = t.y+t.height/2;
+              t.rightMidX = t.x+t.width;
+              t.rightMidY = t.y+t.height/2;
+              t.topMidX = t.x+t.width/2;
+              t.topMidY = t.y;
+              t.bottomMidX = t.x+t.width/2;
+              t.bottomMidY = t.y+t.height;
+              increaseParameter = "";
+              direction = "";
+              factor = 0;
+              lastModifiedTableDimension = null;
+              projectLocalStorage.removeItem("tables");
+              projectLocalStorage.setItem("tables",JSON.stringify(Tables));
+              canvas.onmouseup = null;
+              canvas.onmousemove = null;
+              canvas.removeAttribute("style","cursor:move");
+              draw(ctx);
+              return;
+            }
+          } 
+        }
+      }
+    }
     for(var t of Tables)
     {
     if(cx>=t.x && cx<=t.x+t.width && cx>=t.y && cy<=t.y+t.height)
@@ -121,6 +413,14 @@ function setup(canvasid)
     pos4 = e.clientY;
     t.x=t.x-pos1;
     t.y=t.y-pos2;
+    t.leftMidX = t.x;
+    t.leftMidY = t.y+t.height/2;
+    t.rightMidX = t.x+t.width;
+    t.rightMidY = t.y+t.height/2;
+    t.topMidX = t.x+t.width/2;
+    t.topMidY = t.y;
+    t.bottomMidX = t.x+t.width/2;
+    t.bottomMidY = t.y+t.height;
     }
   }
 
@@ -175,6 +475,14 @@ function setTableDimension(t,f)
     {
       tt.width=Math.ceil(maxWidth)+50;
       tt.height=tt.height+40;
+      tt.leftMidX = tt.x;
+      tt.leftMidY = tt.y+tt.height/2;
+      tt.rightMidX = tt.x+tt.width;
+      tt.rightMidY = tt.y+tt.height/2;
+      tt.topMidX = tt.x+tt.width/2;
+      tt.topMidY = tt.y;
+      tt.bottomMidX = tt.x+tt.width/2;
+      tt.bottomMidY = tt.y+tt.height;
       break;    
     }
     
@@ -484,6 +792,7 @@ document.getElementById("fieldTable").appendChild(tbody);
 
 function highlightRectangle(canvas,event)
 {
+
 mycanvas=canvas;
 if(Tables.length==0) return;
 ctx=canvas.getContext("2d");
@@ -551,6 +860,39 @@ for(var t of Tables)
   ctx.fillRect(t.x-4+t.width,t.y-4+t.height,9,9);
   ctx.fill();
   ctx.stroke();
+  /**
+   * Left X,Left Y Mid
+   */
+  ctx.beginPath();
+  ctx.fillStyle="green";
+  ctx.fillRect(t.leftMidX-4,t.leftMidY-4,9,9);
+  ctx.fill();
+  ctx.stroke();
+    /**
+   * Right X,Right Y Mid
+   */
+  ctx.beginPath();
+  ctx.fillStyle="green";
+  ctx.fillRect(t.rightMidX-4,t.rightMidY-4,9,9);
+  ctx.fill();
+  ctx.stroke();
+  /**
+  * Top X,Top Y Mid
+  */
+  ctx.beginPath();
+  ctx.fillStyle="green";
+  ctx.fillRect(t.topMidX-4,t.topMidY-4,9,9);
+  ctx.fill();
+  ctx.stroke();
+  /**
+  * Bottom X,Bottom Y Mid
+  */
+  ctx.beginPath();
+  ctx.fillStyle="green";
+  ctx.fillRect(t.bottomMidX-4,t.bottomMidY-4,9,9);
+  ctx.fill();
+  ctx.stroke();   
+
   if(t.Fields.length>0)
   {
     let depth = 0;
@@ -808,6 +1150,14 @@ table.y=y;
 table.name="Table "+Tables.length;
 table.width=ctx.measureText(table.name).width+80;
 table.height=80;
+table.leftMidX = x;
+table.leftMidY = y+table.height/2;
+table.rightMidX = x+table.width;
+table.rightMidY = y+table.height/2;
+table.topMidX = x+table.width/2;
+table.topMidY = y;
+table.bottomMidX = x+table.width/2;
+table.bottomMidY = y+table.height;
 Tables.push(table);
 projectLocalStorage.removeItem("tables");
 projectLocalStorage.setItem("tables",JSON.stringify(Tables));
@@ -940,5 +1290,10 @@ function del()
    draw(ctx);
  }
  if (Tables == null || Tables.length ==0) projectLocalStorage.removeItem("tables");
-
 }
+function removeAllTables() {
+  console.log("Remove All invoked");
+  projectLocalStorage.removeItem("tables");
+  draw(ctx);
+}
+
